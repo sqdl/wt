860a960934f28c366561108fa04bd4e1b4c4e55a
sqdl <1819789436@qq.com>
2017-09-26 20:49:25 +0800

    add others

--- admin/inc/script.php
+++
@@ -1,4 +0,0 @@
-
-  <script src="../assets/vendors/nprogress/nprogress.js"></script>
-  <script src="../assets/vendors/jquery/jquery.js"></script>
-  <script src="../assets/vendors/bootstrap/js/bootstrap.js"></script>
